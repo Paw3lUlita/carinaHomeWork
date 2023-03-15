954fe81af001c675104a325b608deb2986b3c3df
Paw3lUlita <pawel.ulita33@gmail.com>
2023-03-15 13:34:12 +0100

    changes

---
+++ API_KEY INFO.md
@@ -0,0 +1,2 @@
+To run API test you need API key, 
+which you should specify in src/main/resources/_config.properties
--- src/main/java/org/example/carina/demo/api/SearchForCollectionMethod.java
+++ src/main/java/org/example/carina/demo/api/SearchForCollectionMethod.java
@@ -8,7 +8,7 @@ import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 import com.zebrunner.carina.utils.Configuration;
 
-@Endpoint(url = "${base_url}/search/collections/?client_id=${api_key}", methodType = HttpMethodType.GET)
+@Endpoint(url = "${base_url}/search/collections/", methodType = HttpMethodType.GET)
 @ResponseTemplatePath(path = "api/searchCollection/_get/rs.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class SearchForCollectionMethod extends AbstractApiMethodV2 {
@@ -16,6 +16,6 @@ public class SearchForCollectionMethod extends AbstractApiMethodV2 {
     public SearchForCollectionMethod() {
 
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
-        replaceUrlPlaceholder("api_key", Configuration.getEnvArg("api_key"));
+        addUrlParameter("client_id", Configuration.getEnvArg("api_key") );
     }
 }
